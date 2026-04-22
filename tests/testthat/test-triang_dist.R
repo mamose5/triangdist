97b6bf0b96a82e6e5b940ee1b0bcd8ada356a81a
mamose5 <mamose5@alumni.uv.es>
2026-04-22 20:13:01 +0200

    Fixed lintr style issues.

--- tests/testthat/test-triang_dist.R
+++ tests/testthat/test-triang_dist.R
@@ -16,8 +16,8 @@ test_that("ptriang works", {
   expect_equal(ptriang(0.5, 0, 1, 0.5), 0.5)
   expect_equal(ptriang(-1, 0, 1, 0.5), 0)
   expect_equal(ptriang(2, 0, 1, 0.5), 1)
-  expect_equal(ptriang(0.3, 0, 1, 0.5), (0.3^2)/(1*0.5))
-  expect_equal(ptriang(0.7, 0, 1, 0.5), 1 - (0.3^2)/(1*0.5))
+  expect_equal(ptriang(0.3, 0, 1, 0.5), (0.3 ^ 2) / (1 * 0.5))
+  expect_equal(ptriang(0.7, 0, 1, 0.5), 1 - (0.3 ^ 2) / (1 * 0.5))
 })
 
 test_that("qtriang works", {
@@ -39,16 +39,16 @@ test_that("rtriang works", {
 
 test_that("vectorization works", {
   expect_equal(
-    dtriang(c(0.2, 0.8), c(0,0), c(1,1), c(0.5,0.5)),
-    c(dtriang(0.2,0,1,0.5), dtriang(0.8,0,1,0.5))
+    dtriang(c(0.2, 0.8), c(0, 0), c(1, 1), c(0.5, 0.5)),
+    c(dtriang(0.2, 0, 1, 0.5), dtriang(0.8, 0, 1, 0.5))
   )
   expect_equal(
-    ptriang(c(0.2, 0.8), c(0,0), c(1,1), c(0.5,0.5)),
-    c(ptriang(0.2,0,1,0.5), ptriang(0.8,0,1,0.5))
+    ptriang(c(0.2, 0.8), c(0, 0), c(1, 1), c(0.5, 0.5)),
+    c(ptriang(0.2, 0, 1, 0.5), ptriang(0.8, 0, 1, 0.5))
   )
   expect_equal(
-    qtriang(c(0.25, 0.75), c(0,0), c(1,1), c(0.5,0.5)),
-    c(qtriang(0.25,0,1,0.5), qtriang(0.75,0,1,0.5))
+    qtriang(c(0.25, 0.75), c(0, 0), c(1, 1), c(0.5, 0.5)),
+    c(qtriang(0.25, 0, 1, 0.5), qtriang(0.75, 0, 1, 0.5))
   )
 })
 
